d23e7fdf539aca08f549618baff2fa8f80c33ee5
Ivan <example@gmail.com>
2021-04-22 18:23:09 +0200

    zadanie 012-clean

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log(1); 
+const numberOfFilms = +prompt('Сколько фильмов выуже посмотрели?',''); 
+
+const personalMooviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMooviesDB.movies[a] = b;     
+personalMooviesDB.movies[c] = d;   
+console.log(personalMooviesDB);
